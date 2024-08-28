d9bc283781f9787b2eb3e43e26f32733296d6212
Chris Cheshire <chris.cheshire@crick.ac.uk>
2024-08-28 09:21:34 +0100

    Update packaging

--- crick_genome_tools/__init__.py
+++ crick_genome_tools/__init__.py
@@ -3,8 +3,8 @@
 Set app version
 """
 
-# from importlib.metadata import version
+from importlib.metadata import version
 
 
 # Set version from package information
-# __version__ = version("crick-genome-tools")
+__version__ = version("crick-genome-tools")
