2b2ffb4ffb8d76c18ffcb4cae12da6837f94bc42
areda elezi <areda.elezi@crick.ac.uk>
2025-04-07 12:17:25 +0100

    extract index from header str

--- crick_genome_tools/seq/barcode_demux.py
+++ crick_genome_tools/seq/barcode_demux.py
@@ -1,2 +1,46 @@
-def test_function():
-    return True
+from crick_genome_tools.io.fastq_file import FastqFile
+
+
+def extract_index_from_header(name: str) -> str:
+    """
+    Extract the index from the FASTQ read header.
+
+    Args:
+        name (str): The read name from the FASTQ file.
+
+    Returns:
+        str: The extracted index.
+    """
+    if not name:
+        raise ValueError("Read header is empty.")
+    if name is None:
+        raise ValueError("Read header is None.")
+
+    # Split the name by spaces and take the last part
+    # this assumes the index is always at the end of the header
+    split_str = name.rsplit(" ", 1)[-1]
+    # Split that last part by colons and join everything after the third colon
+    parts = split_str.split(":")
+    index_part = ":".join(parts[3:])
+
+    # Check for malformed index
+    if index_part.count(":") > 1:
+        raise ValueError("Too many colons in index portion — possibly malformed index.")
+
+    return index_part
+
+
+def read_fastq(self, fastq_file):
+    """
+    Read a FASTQ file and return a list of sequences.
+
+    Args:
+        file_path (str): Path to the FASTQ file.
+
+    Returns:
+        list: List of sequences.
+    """
+    fastq = FastqFile(fastq_file)
+
+    for name, seq, qual in fastq.open_read_iterator(as_string=True):
+        self.extract_index_from_header(name)
--- tests/test_seq_barcode_demux.py
+++ tests/test_seq_barcode_demux.py
@@ -1,10 +1,30 @@
+"""
+Tests covering the barcode_demux module
+"""
 
-import pytest
+# pylint: disable=missing-function-docstring,missing-class-docstring,no-member
+
 from assertpy import assert_that
 
+from crick_genome_tools.seq.barcode_demux import extract_index_from_header
 
-from crick_genome_tools.seq.barcode_demux import test_function
 
 class TestBarcodeDemux:
-    def test_barcode_demux_function(self):
-        assert_that(test_function()).is_true()
+    def test_extract_index_from_header_none(self):
+        assert_that(extract_index_from_header).raises(ValueError).when_called_with(None)
+
+    def test_extract_index_from_header_empty(self):
+        assert_that(extract_index_from_header).raises(ValueError).when_called_with("")
+
+    def test_extract_index_from_header_invalid(self):
+        header = "@LH00442:107:22YHM5LT3:2:1101:1092:1064 2:N:0:TCACCA:GGAC+NCCTTGT:CTC"
+        assert_that(extract_index_from_header).raises(ValueError).when_called_with(header)
+
+    def test_extract_index_from_header_isvalid(self):
+        header = "@LH00442:107:22YHM5LT3:2:1101:1092:1064 2:N:0:TCACCAGGAC+NCCTTGTCTC"
+        header_2 = "@LH00442:107 1:N:0:GCGCTTCTAC+NTCCTTGGCT"
+        header_3 = "invalid_header"
+
+        assert_that(extract_index_from_header(header)).is_equal_to("TCACCAGGAC+NCCTTGTCTC")
+        assert_that(extract_index_from_header(header_2)).is_equal_to("GCGCTTCTAC+NTCCTTGGCT")
+        assert_that(extract_index_from_header(header_3)).is_equal_to("")
