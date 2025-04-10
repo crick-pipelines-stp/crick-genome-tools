529280b58582754ccc9cad0244e14bcace7d1436
Chris Cheshire <chris.cheshire@crick.ac.uk>
2025-04-10 09:25:56 +0100

    Refactor tests to use pytest and assertpy for better readability and consistency

--- tests/test_blast_utils.py
+++ tests/test_blast_utils.py
@@ -5,30 +5,29 @@ Tests for fasta blast utils.
 # pylint: disable=missing-function-docstring,missing-class-docstring
 
 import os
-import unittest
+from assertpy import assert_that
+import pytest
 
 from crick_genome_tools.blast.utils import build_fasta_from_top_hits
-
 
 FASTA_SOURCE_FOLDER = "tests/data/io/fasta"
 BLAST_FOLDER_VALID = "tests/data/blast/valid"
 BLAST_FOLDER_INVALID = "tests/data/blast/invalid"
 
-
-class TestBuildFastaFromTopHits(unittest.TestCase):
+class TestBuildFastaFromTopHits:
     def test_build_fasta_from_top_hits_empty_blast_file(self):
         # Test and Assert
-        with self.assertRaises(ValueError):
-            build_fasta_from_top_hits(os.path.join(FASTA_SOURCE_FOLDER, "extended_valid.fasta"), os.path.join(BLAST_FOLDER_INVALID, "empty.txt"))
+        with pytest.raises(ValueError):
+            assert_that(build_fasta_from_top_hits(os.path.join(FASTA_SOURCE_FOLDER, "extended_valid.fasta"), os.path.join(BLAST_FOLDER_INVALID, "empty.txt")))
 
     def test_build_fasta_from_top_hits_blast_not_found(self):
         # Test and Assert
-        with self.assertRaises(FileNotFoundError):
+        with pytest.raises(FileNotFoundError):
             build_fasta_from_top_hits(os.path.join(FASTA_SOURCE_FOLDER, "extended_valid.fasta"), os.path.join(BLAST_FOLDER_INVALID, "not_found.txt"))
 
     def test_build_fasta_from_top_hits_fasta_not_found(self):
         # Test and Assert
-        with self.assertRaises(FileNotFoundError):
+        with pytest.raises(FileNotFoundError):
             build_fasta_from_top_hits(os.path.join(FASTA_SOURCE_FOLDER, "not_found.fasta"), os.path.join(BLAST_FOLDER_VALID, "viral_top_hits.txt"))
 
     def test_build_fasta_from_top_hits_single(self):
@@ -38,12 +37,9 @@ class TestBuildFastaFromTopHits(unittest.TestCase):
         )
 
         # Assert
-        self.assertTrue("H1N1pdm_PA" in result)
-        self.assertTrue("H1N1pdm_HA" in result)
-        self.assertTrue("H1N1pdm_NA" in result)
-        self.assertTrue("H1N1pdm_NS" in result)
-        self.assertTrue("fake_1" not in result)
-        self.assertTrue("fake_2" not in result)
+        assert_that(result).is_not_empty()
+        assert_that(result).contains("H1N1pdm_PA", "H1N1pdm_HA", "H1N1pdm_NA", "H1N1pdm_NS")
+        assert_that(result).does_not_contain("fake_1", "fake_2")
 
     def test_build_fasta_from_top_hits_multi(self):
         # Test
@@ -52,14 +48,12 @@ class TestBuildFastaFromTopHits(unittest.TestCase):
         )
 
         # Assert
-        self.assertTrue("H1N1pdm_PA" in result)
-        self.assertTrue("H1N1pdm_HA" in result)
-        self.assertTrue("H1N1pdm_NA" in result)
-        self.assertTrue("H1N1pdm_NS" in result)
+        assert_that(result).is_not_empty()
+        assert_that(result).contains("H1N1pdm_PA", "H1N1pdm_HA", "H1N1pdm_NA", "H1N1pdm_NS")
 
     def test_build_fasta_from_top_hits_hit_not_found(self):
         # Test and Assert
-        with self.assertRaises(ValueError):
+        with pytest.raises(ValueError):
             build_fasta_from_top_hits(
                 os.path.join(FASTA_SOURCE_FOLDER, "extended_valid.fasta"), os.path.join(BLAST_FOLDER_INVALID, "hit_not_found.txt")
             )
