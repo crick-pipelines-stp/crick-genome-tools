56ea146cc5eeea5880c60c3a6cb2438247467d3b
Chris Cheshire <chris.cheshire@crick.ac.uk>
2025-04-10 09:53:38 +0100

    Refactor SubprocessStream tests to use pytest and assertpy for improved readability and consistency

--- tests/test_io_subprocess_stream.py
+++ tests/test_io_subprocess_stream.py
@@ -1,49 +1,38 @@
 """
-Subprocess stream tests
+Test the SubprocessStream class.
 """
 
 # pylint: disable=missing-function-docstring,missing-class-docstring
 
-import os
-import unittest
+from assertpy import assert_that
 
 from crick_genome_tools.io.subprocess_stream import SubprocessStream
-from tests.utils import with_temporary_folder
-
 
 CONTENT = "test_content"
 READ_NAME = "@NB501505:171:H3KMGAFX3:1:21208:17616:17963 1:N:0:AGATCTCGGT"
 
 
-class TestSubprocessStream(unittest.TestCase):
-
-    def test_subprocess_stream_gzip_read(self):
-        """Test with read gzip file"""
-
-        # Test and Assert
-        with SubprocessStream(["gzip", "-c", "-d", "tests/data/io/fastq/sc_10k.fastq.gz"], mode="r") as stream:
+class TestSubprocessStream:
+    def test_io_subproc_gzip_read(self):
+        with SubprocessStream(
+            ["gzip", "-c", "-d", "tests/data/io/fastq/sc_10k.fastq.gz"], mode="r"
+        ) as stream:
             for idx, line in enumerate(stream):
                 if idx == 0:
-                    line_str = line.decode("UTF-8").strip()
-                    self.assertEqual(line_str, READ_NAME)
+                    line_str = line.decode("utf-8").strip()
+                    assert_that(line_str).is_equal_to(READ_NAME)
 
-    @with_temporary_folder
-    def test_subprocess_stream_gzip_write(self, tmp_path):
-        """Test with write gzip file"""
+    def test_io_subproc_gzip_write_and_read_back(self, tmp_path):
+        out_file = tmp_path / "test.gz"
 
-        # Setup
-        filename = os.path.join(tmp_path, "test.gz")
-        with open(filename, "w", encoding="UTF-8") as file:
-            stream = SubprocessStream(["gzip", "-c"], stdout=file, mode="w")
-
-            # Test
-            stream.write(CONTENT.encode("UTF-8"))
+        # Write
+        with open(out_file, "w", encoding="utf-8") as f:
+            stream = SubprocessStream(["gzip", "-c"], stdout=f, mode="w")
+            stream.write(CONTENT.encode("utf-8"))
             stream.close()
 
-        # Assert
-        stream = SubprocessStream(["gzip", "-c", "-d", filename], mode="r")
-        assert stream is not None
-
-        for idx, line in enumerate(stream):
-            if idx == 0:
-                self.assertEqual(line.decode("UTF-8"), CONTENT)
+        # Read
+        with SubprocessStream(["gzip", "-c", "-d", str(out_file)], mode="r") as stream:
+            for idx, line in enumerate(stream):
+                if idx == 0:
+                    assert_that(line.decode("utf-8")).is_equal_to(CONTENT)
